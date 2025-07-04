749c7e9cc6782a1d54195f5ae96c50bd807298cd
dan0326 <c0975371883@gmail.com>
2025-07-04 23:21:25 +0800

    new graph example following 3b1b doc

---
+++ graphExample.py
@@ -0,0 +1,7 @@
+from manimlib import *
+
+class GraphEcample(Scene):
+	def construct(self):
+		axes= Axes((-3,10), (-1,8))
+		axes.add_coordinate_labels()
+		self.play(Write(axes, lag_ratio=0.01, run_time=1))
