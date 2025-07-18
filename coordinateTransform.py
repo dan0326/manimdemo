3831269b633bd8f0d6283ff3b27faa7f403027b6
dan0326 <c0975371883@gmail.com>
2025-07-18 14:13:57 +0800

    works to some extent. Current transform method will make the End of vector and dot go very big

--- coordinateTransform.py
+++ coordinateTransform.py
@@ -1,5 +1,15 @@
 from manimlib import *
 
+def complex_func(z):
+    return z**2
+
+def get_complex_vector(plane, z, color=YELLOW):
+    z_vect = Vector()
+    z_vect.put_start_and_end_on(plane.n2p(0), plane.n2p(z))
+    #z_vect.rotate(TAU/4, axis=z_vect.get_vector())
+    z_vect.set_color(color)
+    return z_vect
+
 class OpeningManimExample(Scene):
     def construct(self):
         #add words
@@ -14,7 +24,7 @@ class OpeningManimExample(Scene):
         self.remove(intro_words)
 
         #linear tranasform
-        grid = NumberPlane((-10, 10), (-5, 5))
+        grid = NumberPlane((-25, 25), (-5, 5))
         matrix = [[1, 1], [0, 1]]
         self.play(ShowCreation(grid))
         self.wait()
@@ -22,16 +32,31 @@ class OpeningManimExample(Scene):
         self.wait()
 
         #complex map
-        c_grid = ComplexPlane()
+        self.remove(grid)
+        initail_complex_coord = 3 +4j
+        self.camera.frame.set_height(25)
+        c_grid = ComplexPlane((-25, 25), (-25, 25))
+        z_vect = get_complex_vector(c_grid, initail_complex_coord)
+        z_dot = Dot(radius=0.5)
+        z_dot.set_color(RED)
+        z_dot.f_always.move_to(lambda: z_vect.get_end())
+        self.add(z_dot)
         moving_c_grid = c_grid.copy()
         moving_c_grid.prepare_for_nonlinear_transform()
+        moving_vector = z_vect.copy()
+        moving_dot = z_dot.copy()
+        moving_dot.f_always.move_to(lambda: moving_vector.get_end())
         c_grid.set_stroke(BLUE_E, 1)
         c_grid.add_coordinate_labels(font_size=24)
         self.play(
-            FadeOut(grid),
             Write(c_grid, run_time=3),
-            FadeIn(moving_c_grid))
+            FadeIn(moving_c_grid),
+            FadeOut(c_grid))
         self.wait()
-        self.play(moving_c_grid.animate.apply_complex_function(lambda z: z**2),
+        transformed_complex_coord = complex_func(initail_complex_coord)
+        new_position = c_grid.c2p(transformed_complex_coord.real, transformed_complex_coord.imag)
+        self.play(moving_c_grid.animate.apply_complex_function(complex_func),
+            moving_vector.animate.apply_complex_function(complex_func),
+            moving_dot.animate.apply_complex_function(complex_func),
             run_time=6)
         self.wait()
